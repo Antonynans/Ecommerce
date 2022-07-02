a1b07e89df93103c4b22537e786b6d8b33e3536b
antonynans <antonynans@gmail.com>
2022-07-02 17:38:18 +0100

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Layout } from "../components";
+import { StateContext } from "../context/stateContext";
+import "../styles/globals.css";
+import { Toaster } from 'react-hot-toast';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+      </StateContext>
+    </>
+  );
+}
+
+export default MyApp;
